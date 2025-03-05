22831375851a2a860586268ae12f8b4d7476c043
antonovs105 <antonovs105@protonmail.com>
2025-03-05 08:28:27 +0100

    task done

---
+++ src/Quotation.java
@@ -0,0 +1,7 @@
+public class Quotation {
+    String quote = "I haven't failed. I've just found 10,000 ways that won't work.";
+
+    public void display() {
+        System.out.println(quote);
+    }
+}
